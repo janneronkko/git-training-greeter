e1cdf4dc515825f81a087ebadee8e55f2140f64e
Janne Rönkkö <janne.ronkko@vincit.fi>
2015-02-02 08:33:11 +0200

    Add unit tests for random greeting
    The random greeting feature is now ready for integration

--- testgreeter.py
+++ testgreeter.py
@@ -17,6 +17,7 @@ class TestGreeter( TestCase ):
     self.args = mock.Mock()
     self.args.greeting = mock.sentinel.Greeting
     self.args.name = mock.sentinel.Name
+    self.args.randomGreetin = False
 
     self.greeter = greeter.Greeter( self.args )
     self.greeter._greet = mock.Mock()
@@ -25,4 +26,28 @@ class TestGreeter( TestCase ):
     self.greeter()
 
     self.greeter._greet.assert_called_once_with( mock.sentinel.Greeting, mock.sentinel.Name )
+
+  def testRandomGreeting( self ):
+    self.args.randomGreetin = True
+    self.greeter()
+
+    args, kwargs = self.greeter._greet.call_args
+
+    greeting, name = args
+
+    self.assertTrue( len( kwargs ) == 0 )
+    self.assertNotEqual( greeting, mock.sentinel.Greeting )
+    self.assertEqual( name, mock.sentinel.Name )
+
+  def testRandomGreetingGeneration( self ):
+    calls = 10
+    self.args.randomGreetin = True
+    prevGreeting = self.greeter._greeting()
+    while calls > 0:
+      import time
+      time.sleep( 0.11 ) # Need to sleep more than 0.1 seconds to make sure enough time has passed so that the random greeting is changed
+      greeting = self.greeter._greeting()
+      self.assertNotEqual( prevGreeting, greeting )
+      prevGreeting = greeting
+      calls -= 1
 
