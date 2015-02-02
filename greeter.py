54e3828928537a7d47624bc71947ee3f76f54d3f
Janne Rönkkö <janne.ronkko@vincit.fi>
2015-02-02 07:33:13 +0200

    Add greeting single person

---
+++ greeter.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import argparse
+
+
+def main():
+  args = parseArgs()
+
+  print( 'Hello {name}!'.format( name = args.name ) )
+
+def parseArgs():
+  parser = argparse.ArgumentParser( description = 'Greeter - An Example Application For Vincit Git Trainings' )
+
+  parser.add_argument(
+    'name',
+    nargs = '?',
+    default = 'World',
+  )
+
+  return parser.parse_args()
+
+if __name__ == '__main__':
+  main()
+
